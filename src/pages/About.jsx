d9fa4437b55f858c1113ebfc8a65e60f36a07d6c
EvesLastDescendant <clydewitoutabonnie@gmail.com>
2023-07-19 13:02:37 +0100

    bug fix

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -9,6 +9,7 @@ const About = () => {
         <div className="about">
             <div className="container-fluid-sm">
                 <div className="container-sm mt-5">
+                    <h1>Our Mission and Vision</h1>
                     <div className="row justify-content-between">
                         <div className="col-4 pt-3 border border-primary rounded">
                             <p className="fw-semibold">Mission</p>
@@ -27,7 +28,7 @@ const About = () => {
                 <div className="container-sm shadow rounded-3 mt-5 p-3">
                     <p className="display-6 fw-semibold">Our Training Services</p>
                     <hr />
-                    //<Training />
+                    {/* <Training /> */}
                 </div>
                 <div className="mt-3 float-end shadow rounded rounded-3 bg-primary box">
                     <Link to="/"><FontAwesomeIcon icon={faTentArrowTurnLeft} className="back-to-home-icon" title="Go back home"/></Link>
