d14f3a9a86d52924b6d40bca2d324689430b2a04
EvesLastDescendant <clydewitoutabonnie@gmail.com>
2023-07-20 12:15:58 +0100

    update

--- src/components/Values.jsx
+++ src/components/Values.jsx
@@ -11,35 +11,55 @@ const Values = () => {
                             <FontAwesomeIcon icon={faUsersGear} size="2xl"/>
                             <p>Teamwork</p>
                         </div>
-                        <p>value value value value value</p>
+                        <p>
+                            Lorem ipsum dolor sit, <br /> 
+                            amet consectetur adipisicing elit. <br />
+                            officia quos quis!
+                        </p>
                     </div>
                     <div className="col-4">
                         <div className="circle mx-auto border border-3 border-primary p-1">
                             <FontAwesomeIcon icon={faLockOpen} size="2xl"/>
                             <p>Transparency</p>
                         </div>
-                        <p>value value value value value</p>
+                        <p>
+                            Lorem ipsum dolor sit, <br /> 
+                            amet consectetur adipisicing elit. <br />
+                            officia quos quis!
+                        </p>
                     </div>
                     <div className="col-4">
                         <div className="circle mx-auto border border-3 border-primary p-1">
                             <FontAwesomeIcon icon={faYinYang} size="2xl"/>
                             <p>Respect</p>
                         </div>
-                        <p>value value value value valu</p>
+                        <p>
+                            Lorem ipsum dolor sit, <br />  
+                            amet consectetur adipisicing elit. <br />
+                            officia quos quis!
+                    </p>
                     </div>
                     <div className="col-6">
                         <div className="circle mx-auto border border-3 border-primary p-1">
                             <FontAwesomeIcon icon={faArrowUpRightDots} size="2xl"/>
                             <p>Growth</p>
                         </div>
-                        <p>value value value value valu</p>
+                        <p>
+                            Lorem ipsum dolor sit, <br />
+                            amet consectetur adipisicing elit. <br />
+                            officia quos quis!
+                    </p>
                     </div>
                     <div className="col-6">
                         <div className="circle mx-auto border border-3 border-primary p-1">
                             <FontAwesomeIcon icon={faHourglass} size="2xl"/>
                             <p>Empathy</p>
                         </div>
-                        <p>value value value value valu</p>
+                        <p>
+                            Lorem ipsum dolor sit, <br />  
+                            amet consectetur adipisicing elit. <br />
+                            officia quos quis!
+                    </p>
                     </div>
                 </div>
             </div>
